2b3f248b1dcaea7a76a5680d3e79e608fb7be9a9
TMartin88 <tiernanmartin1@gmail.com>
2022-10-11 17:41:20 +0000

    Create table definition function for writing to txt file

--- run.py
+++ run.py
@@ -30,17 +30,46 @@ def loop_through_worksheets(ws_list):
     Loop through the worksheets
     """
     for wsh in ws_list:
-        get_wsheet_data(wsh)
+        manage_wsheet_data(wsh)
 
 
-def get_wsheet_data(worksheet):
+def manage_wsheet_data(worksheet):
     """
-    Set up the Files to Print using worksheet as title
+    Set up the Files to Write to using worksheet as title
+    Open the file for writing to
     Get all the data from the worksheet
     """
     file_name = f"assets/htmlfiles/{worksheet}.txt"
-
     f_name = open(f'{file_name}', 'w', encoding='utf-8')
+    write_table_definition(f_name)
+    table_data = get_all_data(worksheet)
+
+
+def write_table_definition(txt_file):
+    """
+    Write the definition for the table
+    """
+    lines = ['<figure id="swappera" class="wp-block-table">\n']
+    lines1 = ['<div style="overflow-x: auto;">\n']
+    lines2 = ['<table id="tabletimeA" class="tabletime">\n']
+    lines3 = ['<tbody>\n']
+    lines4 = ['<tr>\n']
+
+    with txt_file:
+        txt_file.writelines(lines)
+        txt_file.writelines(lines1)
+        txt_file.writelines(lines2)
+        txt_file.writelines(lines3)
+        txt_file.writelines(lines4)
+
+
+def get_all_data(data):
+    """
+    Get all the data from the worksheet in a list of lists
+    """
+    list_of_lists = data.get_all_values()
+
+    return list_of_lists
 
 
 def get_the_table_header(all_data):
