cd1a24e9fbd97acb0df9cf95c318bb5beafa7023
TMartin88 <tiernanmartin1@gmail.com>
2022-10-12 08:43:15 +0000

    Introduce 2 functions from thispointer.com See Credits Append single line to txt file Append Multiple Lines to txt file Using these to simplify writing to txt files.

--- run.py
+++ run.py
@@ -67,6 +67,43 @@ def write_table_definition(txt_file):
         txt_file.writelines(lines4)
 
 
+def append_new_line(file_name, text_to_append):
+    """Append given text as a new line at the end of file"""
+    # Open the file in append & read mode ('a+')
+    with open(file_name, "a+", encoding='utf-8') as file_object:
+        # Move read cursor to the start of file.
+        file_object.seek(0)
+        # If file is not empty then append '\n'
+        data = file_object.read(100)
+        if len(data) > 0:
+            file_object.write("\n")
+        # Append text at the end of file
+        file_object.write(text_to_append)
+
+
+def append_multiple_lines(file_name, lines_to_append):
+    """Append given text lines as a new line at the end of file"""
+    # Open the file in append & read mode ('a+')
+    with open(file_name, "a+", encoding='utf-8') as file_object:
+        append_eol = False
+        # Move read cursor to the start of file.
+        file_object.seek(0)
+        # Check if file is not empty
+        data = file_object.read(100)
+        if len(data) > 0:
+            append_eol = True
+        # Iterate over each string in the list
+        for line in lines_to_append:
+            # If file is not empty then append '\n' before first line for
+            # other lines always append '\n' before appending line
+            if append_eol is True:
+                file_object.write("\n")
+            else:
+                append_eol = True
+            # Append element at the end of file
+            file_object.write(line)
+
+
 def loop_th_headings(header_data):
     """
     Loop through the th headings
