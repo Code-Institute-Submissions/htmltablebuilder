91ce20dbac8b25522dd5da74091fcc4e99f5e040
TMartin88 <tiernanmartin1@gmail.com>
2022-10-12 09:48:53 +0000

    Write th header function now using append_multiple_lines function Amend append_multiple_lines function to handle complex lists and simple lists

--- run.py
+++ run.py
@@ -40,13 +40,10 @@ def manage_wsheet_data(worksheet):
     Get all the data from the worksheet
     """
     file_name = f"assets/htmlfiles/{worksheet}.txt"
-    # f_name = open(f'{file_name}', 'w', encoding='utf-8')
-    write_table_definition(file_name)
+    # write_table_definition(file_name)
     table_data = get_all_data(worksheet)
     th_heading_data = get_the_table_header(table_data)
-    th_heading_loop = loop_th_headings(th_heading_data)
-    # f_name = open(f'{file_name}', 'w', encoding='utf-8')
-    # write_table_th(f_name, th_heading_loop)
+    write_table_th(file_name, th_heading_data)
 
 
 def write_table_definition(txt_file):
@@ -68,48 +65,12 @@ def write_table_definition(txt_file):
     lines4 = ['<tr>']
     table_defs.append(lines4)
 
-    append_multiple_lines(txt_file, table_defs)
+    string_type = "complexList"
+
+    append_multiple_lines(txt_file, table_defs, string_type)
 
 
-def append_new_line(file_name, text_to_append):
-    """Append given text as a new line at the end of file"""
-    # Open the file in append & read mode ('a+')
-    with open(file_name, "a+", encoding='utf-8') as file_object:
-        # Move read cursor to the start of file.
-        file_object.seek(0)
-        # If file is not empty then append '\n'
-        data = file_object.read(100)
-        if len(data) > 0:
-            file_object.write("\n")
-        # Append text at the end of file
-        file_object.write(text_to_append)
-
-
-def append_multiple_lines(file_name, lines_to_append):
-    """Append given text lines as a new line at the end of file"""
-    # Open the file in append & read mode ('a+')
-    with open(file_name, "a+", encoding='utf-8') as file_object:
-        append_eol = False
-        # Move read cursor to the start of file.
-        file_object.seek(0)
-        # Check if file is not empty
-        data = file_object.read(100)
-        if len(data) > 0:
-            append_eol = True
-        # Iterate over each string in the list
-        for line in lines_to_append:
-            # If file is not empty then append '\n' before first line for
-            # other lines always append '\n' before appending line
-            if append_eol is True:
-                file_object.write("\n")
-            else:
-                append_eol = True
-            # Append element at the end of file
-            line_txt = line[0]
-            file_object.write(line_txt)
-
-
-def loop_th_headings(header_data):
+def write_table_th(txt_file, header_data):
     """
     Loop through the th headings
     We watch out for colspan requirements in the head data
@@ -142,20 +103,53 @@ def loop_th_headings(header_data):
 
         k = 0
 
-    return thisth
+    thisrvth = thisth.copy()
+    thisrvth.reverse()
+    string_type = "simpleList"
+
+    append_multiple_lines(txt_file, thisrvth, string_type)
 
 
-def write_table_th(txt_file, th_data):
-    """
-    Write the th's for the table
-    """
-    open_tr = "<tr>"
-    txt_file.write(f'{open_tr}\n')
-    for each_th in reversed(th_data):
-        txt_file.write(f'{each_th}\n')
+def append_new_line(file_name, text_to_append):
+    """Append given text as a new line at the end of file"""
+    # Open the file in append & read mode ('a+')
+    with open(file_name, "a+", encoding='utf-8') as file_object:
+        # Move read cursor to the start of file.
+        file_object.seek(0)
+        # If file is not empty then append '\n'
+        data = file_object.read(100)
+        if len(data) > 0:
+            file_object.write("\n")
+        # Append text at the end of file
+        file_object.write(text_to_append)
 
-    close_tr = "<\\tr>"
-    txt_file.write(close_tr)
+
+def append_multiple_lines(file_name, lines_to_append, ls_type):
+    """Append given text lines as a new line at the end of file"""
+    # Open the file in append & read mode ('a+')
+    with open(file_name, "a+", encoding='utf-8') as file_object:
+        append_eol = False
+        # Move read cursor to the start of file.
+        file_object.seek(0)
+        # Check if file is not empty
+        data = file_object.read(100)
+        if len(data) > 0:
+            append_eol = True
+        # Iterate over each string in the list
+        for line in lines_to_append:
+            # If file is not empty then append '\n' before first line for
+            # other lines always append '\n' before appending line
+            if append_eol is True:
+                file_object.write("\n")
+            else:
+                append_eol = True
+            # Append element at the end of file
+            if ls_type == "simpleList":
+                line_txt = line
+            else:
+                line_txt = line[0]
+
+            file_object.write(line_txt)
 
 
 def get_all_data(data):
